4cdf0ff11d727564b815ff9801343a24b31232ce
MirGerStudent <mirger140503@gmail.com>
2025-06-10 14:30:53 +0600

    [Service] delete auth checking

--- src/main/java/plant_nursery/app/PlantNursery/core/service/EventService.java
+++ src/main/java/plant_nursery/app/PlantNursery/core/service/EventService.java
@@ -1,20 +1,14 @@
 package plant_nursery.app.PlantNursery.core.service;
 
-import io.grpc.Context;
-import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 import org.lognet.springboot.grpc.GRpcService;
 import org.springframework.beans.factory.annotation.Autowired;
-import plant_nursery.app.PlantNursery.core.auth.AuthInterceptor;
 import plant_nursery.app.PlantNursery.core.auth.Constant;
-import plant_nursery.app.PlantNursery.core.auth.JwtUtil;
 import plant_nursery.app.PlantNursery.core.repository.EventRepository;
 import protobuf.*;
 
 @GRpcService
 public class EventService extends EventServiceGrpc.EventServiceImplBase {
-    private static final Context.Key<String> ROLE_KEY = Context.key("role");
-
     @Autowired
     EventRepository eventRepository;
 
@@ -41,12 +35,8 @@ public class EventService extends EventServiceGrpc.EventServiceImplBase {
 
     @Override
     public void deleteEvent(DeleteEventRequest request, StreamObserver<Empty> responseObserver) {
-//        if (Constant.adminCheck(ROLE_KEY, responseObserver)) {
-
-        if (Constant.adminCheck(String.valueOf(AuthInterceptor.USER_ROLE), responseObserver)) {
-            eventRepository.DeleteEvent(request);
-            responseObserver.onNext(Empty.newBuilder().build());
-            responseObserver.onCompleted();
-        }
+        eventRepository.DeleteEvent(request);
+        responseObserver.onNext(Empty.newBuilder().build());
+        responseObserver.onCompleted();
     }
 }
