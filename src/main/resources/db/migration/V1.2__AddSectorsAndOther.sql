6cca8502fa0a83288395317cd6fdefc48c59a7a9
MirGerStudent <mirger140503@gmail.com>
2025-06-22 08:31:26 +0600

    [DataBase] add new migration for create new entity

---
+++ src/main/resources/db/migration/V1.2__AddSectorsAndOther.sql
@@ -0,0 +1,18 @@
+-- Добавление новых секторов
+INSERT INTO Sector (parent_id, name) VALUES
+(1, 'Школа 1'),
+(4, 'Поле 1'), (4, 'Поле 2'), (4, 'Поле 3'),
+(4, 'Поле 4'), (4, 'Поле 5'), (4, 'Поле 6'),
+(4, 'Поле 7'), (4, 'Поле 8'), (4, 'Поле 9');
+
+-- Добавление нового растения
+INSERT INTO Plant (height_stem_min, height_stem_max, width_stem_min, width_stem_max, spice, sort, description) VALUES
+(2, 18, 1, 4, 'Ива', 'Ива обыкновенная', 'Самая крутая обыкновенная ива'),
+(1, 3, 1, 3, 'Древко', 'Древко обыкновенное', 'Самое крутое обыкновенное древко');
+
+-- Добавление растений на поля
+INSERT INTO SectorPlant (plant_id, plant_count, planting_date) VALUES
+(2, 130, NOW()),
+(2, 130, NOW()),
+(3, 130, NOW()),
+(3, 130, NOW());
